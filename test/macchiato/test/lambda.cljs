8d28296b3c2666dd01d80ba010c25f0d7c7fc16e
Jelle Akkerman <jellea@users.noreply.github.com>
2018-01-03 10:53:16 +0100

    checking

--- test/macchiato/test/lambda.cljs
+++ test/macchiato/test/lambda.cljs
@@ -1,2 +1,8 @@
 (ns macchiato.test.lambda
-  (:require [clojure.test :refer :all]))
+  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]))
+
+(deftest request-transform
+  (is true))
+
+(deftest response-transform
+  (is true))
